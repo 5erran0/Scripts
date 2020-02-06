6d398e4dcca31cc9ee48ed3290f5a7423eaad5ef
Gerard Serrano <gerard.serrano.ginebra@gmail.com>
2020-02-06 14:26:38 +0000

    Add files via upload

---
+++ welcome_party.sh
@@ -0,0 +1,7 @@
+# Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+upgrade_oh_my_zsh
